c34532ed246c936746ccc4dc10472aee914ab855
rDRANSKI <ronaldodranskidranski988@gmail.com>
2026-04-14 19:22:11 -0300

    Update main.dart

--- Atividade2/main.dart
+++ Atividade2/main.dart
@@ -41,3 +41,41 @@ void cadastrar() {
   biblioteca.add(Livro(proximoId++, titulo, autor, ano));
   print('Livro cadastrado com sucesso!');
 }
+
+void listar() {
+  if (biblioteca.isEmpty) {
+    print('Nenhum livro cadastrado no sistema.');
+  } else {
+    biblioteca.forEach((l) => print(l));
+  }
+}
+
+void atualizar() {
+  stdout.write('Digite o ID do livro par atualizar: ');
+  int id = int.parse(stdin.readLineSync()!);
+
+  try {
+    var livro = biblioteca.firstWhere((l) => l.id == id);
+
+    stdout.write('Novo Título (atual: ${livro.titulo}): ');
+    livro.titulo = stdin.readLineSync()!;
+    print('Livro atualizado com sucesso!');
+  } catch (e) {
+    print('Livro não encontrado.');
+  }
+}
+
+void remover() {
+  stdout.write('Digite o ID do livro para remover: ');
+  int id = int.parse(stdin.readLineSync()!);
+
+  int tamanhoInicial = biblioteca.length;
+
+  biblioteca.removeWhere((l) => l.id == id);
+
+  if (biblioteca.length < tamanhoInicial) {
+    print('Livro removido com sucesso!');
+  } else {
+    print('ID não encontrado. Nada foi removido.');
+  }
+}
